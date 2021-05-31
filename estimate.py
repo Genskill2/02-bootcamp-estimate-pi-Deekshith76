e5f718d2c1adbefb0f44c997d5b31f04fba5433e
Deekshith <84832784+Deekshith76@users.noreply.github.com>
2021-05-31 15:27:29 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -28,13 +28,13 @@ class TestMC(unittest.TestCase):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
         
- def wallis(n):
+def wallis(n):
     product = 1
     for i  in range(1,n+1):
         val = (4*(i**2))/((4*(i**2))-1)
         product *= val
-        pi_val = 2*product
-        return pi_val
+    pi_val = 2*product
+    return pi_val
  
 
 def monte_carlo(n):
